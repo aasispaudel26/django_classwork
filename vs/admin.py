522d12a95b2f31f66f07bce1441d7ca3d6ab75d6
Aasis <aasispaudel@26gmail.com>
2024-07-31 20:45:29 +0545

    model

--- vs/admin.py
+++ vs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from vs.models import person
+class personAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','age', 'email', 'dob','is_married')
+    
+admin.site.register(person,personAdmin)
 # Register your models here.
--- vs/models.py
+++ vs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class person(models.Model):
+  first_name = models.CharField(max_length=15)
+  last_name = models.CharField(max_length=15)
+  age = models.PositiveIntegerField()
+  email = models.EmailField()
+  dob = models.DateField()
+  is_married = models.BooleanField(default=False)
