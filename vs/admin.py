646655dc88873cc5892f2396fa6c595e2bf10f4e
Aasis <aasispaudel@26gmail.com>
2024-08-01 20:00:30 +0545

    relations

--- vs/admin.py
+++ vs/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from vs.models import person
+from vs.models import citizenship, children
+
 class personAdmin(admin.ModelAdmin):
     list_display = ('first_name','last_name','age', 'email', 'dob','is_married')
     
 admin.site.register(person,personAdmin)
+
+class citizenAdmin(admin.ModelAdmin):
+    list_display = ('citizenship_no','person')
+    
+admin.site.register(citizenship, citizenAdmin)
+
+class childrenAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','age', 'parents')
+    
+admin.site.register(children,childrenAdmin)
 # Register your models here.
--- vs/models.py
+++ vs/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
@@ -8,3 +9,23 @@ class person(models.Model):
   email = models.EmailField()
   dob = models.DateField()
   is_married = models.BooleanField(default=False)
+  
+  def __str__(self):
+    return self.first_name
+  
+class citizenship(models.Model):
+  citizenship_no = models.IntegerField()
+  person = models.OneToOneField(person, on_delete = models.CASCADE)
+  
+def __str__(self):
+    return self.person
+  
+  
+class children(models.Model):
+  first_name = models.CharField(max_length=15)
+  last_name = models.CharField(max_length=15)
+  age = models.IntegerField()
+  parents = models.ForeignKey(person, on_delete= models.SET_NULL, null= True)
+
+def __str__(self):
+    return self.first_name
